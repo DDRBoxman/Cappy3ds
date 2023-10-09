a471e89220e1d4d8688c7e5a457cf5c1c2c62992
Colin Edwards <colin@recursivepenguin.com>
2023-10-08 21:03:52 -0500

    Remove changes to main in cappy

--- Cappy3ds/src/main.rs
+++ Cappy3ds/src/main.rs
@@ -1,9 +1,5 @@
 mod capture;
-mod render;
-use futures::executor;
-pub use render::render::State;
 
 fn main() {
-   // let mut res = State::new_from_visual();
-   // let mut v = executor::block_on(res);
+    capture::do_capture();
 }
